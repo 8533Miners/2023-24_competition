681671f3d6d4f811bb8d50ec811bbea31d73c0c3
8533 Miners <8533Miners@gmail.com>
2023-10-04 19:55:23 -0400

    feat: incorporate meepmeep and initial trajectory

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepClass.java
@@ -0,0 +1,35 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepClass {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(20, 30, Math.toRadians(180), Math.toRadians(180), 13.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(12, 56, Math.toRadians(270)))
+                                .forward(12) // Robot drives forward
+                                .turn(Math.toRadians(-30)) // robot pivots
+                                .waitSeconds(3) // do you Identify Prop
+                                .turn(Math.toRadians(30)) // rotate robot towards center
+                                .forward(12)
+                                .waitSeconds(3) // place purple pixle
+                                .turn(Math.toRadians(90)) // rotate towards backdrop
+                                .forward(39)
+                                .waitSeconds(3) // place yellow pixel
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
