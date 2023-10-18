564f2a75cb10e169e5e675505a8a52dd8677bac2
8533 Miners <8533Miners@gmail.com>
2023-10-18 19:17:29 -0400

    feat: add gripper logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Gripper.java
@@ -6,27 +6,29 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Gripper {
     private Servo claw;
-    //private Servo pusher;
+    private Servo pusher;
     private final double CLAW_MIN_PERCENT = 0.0;
     private final double CLAW_MAX_PERCENT = 1.0;
+    private final double PUSHER_UP_POSITION = 0.0;
+    private final double PUSHER_DOWN_POSITION = 1.0;
 
 
     public Gripper(HardwareMap hardwareMap) {
         claw = hardwareMap.get(Servo.class, "claw");
-        //pusher = hardwareMap.get(Servo.class, "pusher");
+        pusher = hardwareMap.get(Servo.class, "pusher");
 
-        claw.scaleRange(CLAW_MIN_PERCENT,CLAW_MAX_PERCENT);
-        //pusher.scaleRange(PUSHER_MIN_PERCENT,PUSHER_MAX_PERCENT);
+        //claw.scaleRange(CLAW_MIN_PERCENT,CLAW_MAX_PERCENT);
+        pusher.scaleRange(0.0,1.0);//PUSHER_MIN_PERCENT,PUSHER_MAX_PERCENT);
     }
 
     public void update(float gripper_close_percent, boolean pusher_down) {
         claw.setPosition(gripper_close_percent);
 
-//        if(pusher_down) {
-//            pusher.setPosition(PUSHER_DOWN_POSITION);
-//        } else {
-//            pusher.setPosition(PUSHER_UP_POSITION);
-//        }
+        if(pusher_down) {
+            pusher.setPosition(PUSHER_DOWN_POSITION);
+        } else {
+            pusher.setPosition(PUSHER_UP_POSITION);
+        }
     }
 
 }
