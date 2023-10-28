6fff50eb49f954b25b1b745d5f09fcf635048a74
rise <mkblaser@users.noreply.github.com>
2023-10-28 13:45:24 -0400

    Update Testing_Elevator_Encoder.java
    chore: bug fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Testing_Elevator_Encoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Testing_Elevator_Encoder.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.subsystems.Elevator;
 
+@TeleOp(group = "TeleOp")
 public class Testing_Elevator_Encoder extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
